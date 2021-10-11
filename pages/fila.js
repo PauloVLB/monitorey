a94ca0d1f09f6196e276ce235ef96a81bbff041d
Isaac Lourenço <isaacmarlondasilvalourenco@gmail.com>
2021-10-11 15:04:28 -0300

    feat(components): PopupDuvida enviado duvida via socket (sem nome aluno)

--- pages/fila.js
+++ pages/fila.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
+import { PopupDuvida } from '../components/PopupDuvida';
 
 const socket = io("http://localhost:8000", { transports: ['websocket'] });
 
 export default function Fila() {
   const [alunos, setAlunos] = useState([]);
+  const [isPopupDuvidaVisivel, setIsPopupDuvidaVisivel] = useState(false);
   
   socket.on('listar', data => {
       setAlunos(data.alunos);
@@ -16,12 +18,23 @@ export default function Fila() {
 
   return(
     <div>
+      <button 
+        onClick={() => setIsPopupDuvidaVisivel(true)}
+        className="bg-green-700 w-full p-4 rounded-md border-2 border-green-600 text-center"
+      >
+        Adicionar dúvida
+      </button>
       {alunos.map((aluno, index) => (
           <div key={index}>
             <label>{`${index+1}. ${aluno.nome}`}</label>
             <button onClick={() => handleExcluir(index)}>Exlcuir</button>
           </div>
         ))}
+      <PopupDuvida
+        isVisivel={isPopupDuvidaVisivel}
+        setIsVisivel={setIsPopupDuvidaVisivel}
+        socket={socket}
+      />
     </div>
   );
 }
